8a89e4dfa8afa3ecadd3b5d8549edac23120a4e7
Kamil Kiklica <kamil.kiklica@gmail.com>
2019-08-21 10:41:49 +0200

    test

--- scripts.js
+++ scripts.js
@@ -1,21 +1,34 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+    console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('authorize_button', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
     });
 }
+// function onSignIn(googleUser) {
+//     var profile = googleUser.getBasicProfile();
+//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//     console.log('Name: ' + profile.getName());
+//     console.log('Image URL: ' + profile.getImageUrl());
+//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// }
+
+
 
 
 // Client ID and API key from the Developer Console
-var CLIENT_ID = '342126350355-ii1j6dp3uv3llkqur635pmbvdn59huv0.apps.googleusercontent.com';
+var CLIENT_ID = '660276360583-8s7r9b170mlrhr93cnka2ea1e14nk3al.apps.googleusercontent.com';
 var API_KEY = 'AIzaSyB23haIBOyfvbZgafkm4jNZMhql-Wr2-QE';
 
 // Array of API discovery doc URLs for APIs used by the quickstart
