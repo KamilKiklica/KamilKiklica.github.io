155b8d06aacc28478fba743095e0b37335b3618f
Kamil Kiklica <kamil.kiklica@gmail.com>
2019-08-22 10:50:27 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,60 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    listFiles();
+}
+
+
+function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+}
+
+/**
+ * Print filess.
+ */
+
+var trackList = [];
+var namesList = [];
+var artWorkList = [];
+function listFiles() {
+    gapi.client.drive.files.list({
+        'pageSize': 100,
+        'fields': "nextPageToken, files(id, name)"
+    }).then(function(response) {
+        appendPre('Files:');
+        var files = response.result.files;
+        if (files && files.length > 0) {
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                var regex = new RegExp("\\w*.mp3$");
+                if (regex.test(file.name)) {
+                    // appendPre(file.name + ' (' + file.id + ')');
+                    const p = document.createElement("a");
+                    p.href = 'https://docs.google.com/uc?export=download&id='+ file.id;
+                    p.textContent = file.name;
+                    document.querySelector('#content').appendChild(p);
+                    var br = document.createElement("br");
+                    document.querySelector('#content').appendChild(br);
+                    var link = 'https://docs.google.com/uc?export=download&id='+ file.id;
+                    trackList.push(link);
+                    namesList.push(file.name);
+                    artWorkList.push('_1');
+                }
+            }
+        } else {
+            appendPre('No files found.');
+        }
+    });
+
+}
+
+
+
 /**
  *
  * HTML5 Audio player with playlist
@@ -9,7 +66,7 @@
  * http://www.script-tutorials.com/
  */
 jQuery(document).ready(function() {
-    listFiles();
+
     // inner variables
     var song;
     var tracker = $('.tracker');
@@ -134,167 +191,3 @@ jQuery(document).ready(function() {
         stop: function(event,ui) {}
     });
 });
-
-
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '660276360583-8s7r9b170mlrhr93cnka2ea1e14nk3al.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyB23haIBOyfvbZgafkm4jNZMhql-Wr2-QE';
-
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
-
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-    gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-    }).then(function () {
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-        // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
-    }, function(error) {
-        appendPre(JSON.stringify(error, null, 2));
-    });
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
-        listFiles();
-    } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
-    }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-}
-
-
-
-
-
-function appendPre(message) {
-    var pre = document.getElementById('content');
-    var textContent = document.createTextNode(message + '\n');
-    pre.appendChild(textContent);
-}
-
-/**
- * Print filess.
- */
-
-// var trackList = [];
-// var namesList = [];
-// var artWorkList = [];
-// function listFiles() {
-//     gapi.client.drive.files.list({
-//         'pageSize': 100,
-//         'fields': "nextPageToken, files(id, name)"
-//     }).then(function(response) {
-//         appendPre('Files:');
-//         var files = response.result.files;
-//         if (files && files.length > 0) {
-//             for (var i = 0; i < files.length; i++) {
-//                 var file = files[i];
-//                 var regex = new RegExp("\\w*.mp3$");
-//                 if (regex.test(file.name)) {
-//                     // appendPre(file.name + ' (' + file.id + ')');
-//                     const p = document.createElement("li");
-//                     p.audiourl = 'https://docs.google.com/uc?export=download&id='+ file.id;
-//                     p.cover = "cover1.jpg";
-//                     p.artist = file.name;
-//                     // p.href = 'https://docs.google.com/uc?export=download&id='+ file.id;
-//                     p.textContent = file.name;
-//                     document.querySelector('#content').appendChild(p);
-//                     // var br = document.createElement("br");
-//                     // document.querySelector('.playlist hidden').appendChild(br);
-//                     // var link = 'https://docs.google.com/uc?export=download&id='+ file.id;
-//                     // trackList.push(link);
-//                     // namesList.push(file.name);
-//                     // artWorkList.push('_1');
-//                 }
-//             }
-//         } else {
-//             appendPre('No files found.');
-//         }
-//     });
-//
-// }
-
-
-
-
-var trackList = [];
-var namesList = [];
-var artWorkList = [];
-function listFiles() {
-    gapi.client.drive.files.list({
-        'pageSize': 100,
-        'fields': "nextPageToken, files(id, name)"
-    }).then(function(response) {
-        appendPre('Files:');
-        var files = response.result.files;
-        if (files && files.length > 0) {
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
-                var regex = new RegExp("\\w*.mp3$");
-                if (regex.test(file.name)) {
-                    // appendPre(file.name + ' (' + file.id + ')');
-                    const p = document.createElement("a");
-                    p.href = 'https://docs.google.com/uc?export=download&id='+ file.id;
-                    p.textContent = file.name;
-                    document.querySelector('#content').appendChild(p);
-                    var br = document.createElement("br");
-                    document.querySelector('#content').appendChild(br);
-                    var link = 'https://docs.google.com/uc?export=download&id='+ file.id;
-                    trackList.push(link);
-                    namesList.push(file.name);
-                    artWorkList.push('_1');
-                }
-            }
-        } else {
-            appendPre('No files found.');
-        }
-    });
-
-}
